a217101416b28265aebf4f06285842a7ee3c1354
sirlilpanda <me@sirlilpanda.studio>
2025-07-08 16:22:22 +1200

    added zon files to zyntax

--- panda_language_zig.lua
+++ panda_language_zig.lua
@@ -3,7 +3,7 @@ local syntax = require "core.syntax"
 
 syntax.add {
   name = "Zig",
-  files = { "%.zig$" },
+  files = { "%.zig$", "%.zig.zon" },
   comment = "//",
   patterns = {
     { pattern = "//.-\n",                       type = "comment"  },
